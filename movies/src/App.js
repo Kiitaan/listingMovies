71bfe2f2e0c23102b3b713fdd4c344eea6e63dda
Kiitaan <christian.lim@outlook.fr>
2018-06-27 18:18:45 +0200

    fetch the movies

--- movies/src/App.js
+++ movies/src/App.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+state = {
+   movies: []
+ }
+
+  componentDidMount() {
+   fetch('https://raw.githubusercontent.com/wildcodeschoolparis/datas/master/movies.json')
+      .then(res => res.json())
+      .then(datas => this.setState({ movies: datas.movies }))
+  }
+
   render() {
+
+    console.log('render', this.state)
+
     return (
+
       <div className="App">
         
       </div>
