d6bebdae7ae965a7d1c00888ffc076ae5477a7f4
Kiitaan <christian.lim@outlook.fr>
2018-06-28 12:42:47 +0200

    remove favorite

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Movie from './components/Movie.js';
-import Favorite from './components/Favorite.js'
-
+import Favorite from './components/Favorite.js';
 import './App.css';
 
 class App extends Component {
@@ -19,9 +18,14 @@ state = {
       .then(datas => this.setState({ movies: datas.movies }))
   }
 
-  addMovie = movieName => {
-    const newMovie = this.state.movies.find(movie => movie.title === movie.title)
+  addMovie = (movieName) => {
+    const newMovie = this.state.movies.find(movie => movie.title === movieName)
     this.setState({ favorites: [newMovie, ...this.state.favorites]})
+  }
+
+  removeMovie = (movieName) => {
+    const notFav = this.state.favorites.filter(favorites => favorites.title !== movieName)
+    this.setState({ favorites: notFav })
   }
 
   render() {
@@ -30,8 +34,7 @@ state = {
 
     const showMovie = this.state.movies.map(movie => <Movie key={movie.id} title={movie.title} year={movie.year} image={movie.posterUrl} addM={this.addMovie}/>)
 
-
-    const showFavorite = this.state.favorites.map(favorite => <Favorite name = {favorite.title} key={favorite.id} image={favorite.posterUrl}/>)
+    const showFavorite = this.state.favorites.map(favorite => <Favorite name={favorite.title} key={favorite.id} year={favorite.year} image={favorite.posterUrl} remove={this.removeMovie}/>)
 
     return ( 
 
--- movies/src/components/Favorite.js
+++ movies/src/components/Favorite.js
@@ -2,13 +2,17 @@ import React from 'react';
 import '../App.css';
 
 const Favorite = props => 
-	{
-		return(		
+{
+	return	(		
 		<div>
-			<div className="title">{props.name}</div>
-        	<img src={props.image} alt="photo"/>
-		</div>
-		)
-	}
 
-export default Favorite;
+			<img src={props.image} alt="film"/>
+			<div className="title">{props.name}</div>
+        	<h4>{props.year}</h4>
+        	<button onClick= {() => props.remove(props.name)}>REMOVE</button>
+
+		</div>
+			)
+}
+
+export default Favorite
